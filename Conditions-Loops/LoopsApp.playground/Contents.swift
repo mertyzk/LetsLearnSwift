9c7e5ca3a69f17d933102be309b8170e1a9fe7e8
RinKusChi <meyuzak@gmail.com>
2022-01-29 23:34:07 +0300

    Update Contents.swift

--- Conditions-Loops/LoopsApp.playground/Contents.swift
+++ Conditions-Loops/LoopsApp.playground/Contents.swift
@@ -30,3 +30,46 @@ while counter <= 3
     print(counter)
     counter += 1
 }
+
+// EXAMPLES...
+
+for i in 3...6{
+    print("Loop 1 : \(i)")
+}
+
+let start1 = 0
+let finish1 = 8
+let up1 = 1
+
+for x in stride(from: start1, through: finish1, by: up1){
+    print("Stride Loop \(x)")
+}
+
+var name = "TestName"
+
+for n in 1...5{
+    print("\(n).\(name)")
+}
+ 
+var numberOfName = name.count
+
+for y in 1...numberOfName{
+    print("\(y).\(name)")
+}
+
+
+// BREAK & CONTINUE
+
+for i in 24...30{
+    if i == 27{
+        break
+    }
+    print("Break Loop \(i)")
+}
+
+for p in 13...19{
+    if p == 17{
+        continue
+    }
+    print("Continue loop \(p)")
+}
