a1aa954bad89cf48cde353326d5049c1a6106ccf
RinKusChi <meyuzak@gmail.com>
2022-01-31 16:59:12 +0300

    Update Contents.swift

--- Collections/CollectionsApp.playground/Contents.swift
+++ Collections/CollectionsApp.playground/Contents.swift
@@ -104,7 +104,7 @@ print(evenNumbers)
 
 // Random number creation
 
-var numbers5 = [Int]()
+var numbers5 = [Int]() // null array
 
 for _ in 1...10{
     let randomNumber = arc4random_uniform(10)
@@ -120,3 +120,110 @@ for _ in 1...6{
     numbers6.append(randomNumber2)
 }
 print(numbers6)
+
+// Example
+
+var points = [Int]()
+var lessons = [String]()
+
+lessons.append("Math")
+points.append(65)
+
+lessons.append("Physics")
+points.append(70)
+
+lessons.append("Biology")
+points.append(89)
+
+lessons.append("History")
+points.append(44)
+
+lessons.append("TestLesson")
+points.append(22)
+
+var top = 0
+
+for i in 0...(lessons.count-1){
+    print("\(lessons[i]) : \(points[i])")
+    top += points[i]
+}
+
+print("Ort. : \(top/lessons.count)")
+
+
+// NULL OR NOT NULL EXAMPLE
+
+var names = ["Ahmet","John","Ali","Ayşe","Navas"]
+var controlNames = "Ali"
+
+for n in names{
+    n == controlNames ? print("True") : print("False")
+}
+
+// SET
+
+var testSet = Set <Int>() // NULL INT SET
+var testSetString: Set = ["X","Y","Z","W","T"]
+var testSet2: Set<String> = ["A","B","C","D"]
+
+// SET OPERATIONS
+
+testSet2.insert("E")
+testSet.isEmpty // null or not null check
+testSet.contains(10) // 10 == true or false
+testSet.sorted() // sorted
+testSet.remove(15) // delete from
+
+testSet.insert(10)
+testSet.insert(25)
+
+for t in testSet{
+    print(t)
+}
+
+// a intersection b = a kesişim b
+// a union b = a birleşim b
+// a symmetricDiffrence b = birleşim - kesişim
+// a substracting b = a fark b
+
+let tekler:Set = [1,3,5,7,9]
+let ciftler:Set = [0,2,4,6,8]
+
+let dizi1 = tekler.union(ciftler).sorted() // a birleşim b
+print(dizi1)
+
+let dizi2 = tekler.intersection(ciftler).sorted() // a kesişim b
+print(dizi2)
+
+let dizi3 = tekler.symmetricDifference(ciftler).sorted() // birleşim - kesişim
+print(dizi3)
+
+let dizi4 = tekler.subtracting(ciftler).sorted() // a fark b
+print(dizi4)
+
+// DICTIONARY
+
+var dic1 = [Int:String]() // key - value
+var dic2 = [3.14:"Pi",2.71:"e"]
+var dic3:[Int:String] = [1:"Bir",2:"İki",3:"Üç"]
+var sehirler = [06:"Ankara",01:"Adana",18:"Çankırı",35:"İzmir"]
+sehirler[34] = "İstanbul"
+sehirler[16] = "Bursa"
+
+print(sehirler)
+
+for (key,value) in sehirler{
+    print(key,value)
+}
+
+// DICTIONARY TO ARRAY && ARRAY TO DICTIONARY
+
+var dersler = ["Kimya","Biyoloji","Matematik"]
+var notlar = [10,20,30]
+var dersnotlar = Dictionary(uniqueKeysWithValues: zip(dersler, notlar))
+print(dersnotlar)
+
+var dersler1 = dersnotlar.keys
+var notlar1 = dersnotlar.values
+print(dersler1)
+print(notlar1)
