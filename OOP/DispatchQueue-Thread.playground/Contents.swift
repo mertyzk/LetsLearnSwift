45788f62a96f48f96563ec0f520bee39189e3f68
RinKusChi <meyuzak@gmail.com>
2022-02-01 19:33:01 +0300

    Thread

---
+++ OOP/DispatchQueue-Thread.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+let queue1 = DispatchQueue(label: "etiket1",qos: DispatchQoS.userInitiated)
+let queue2 = DispatchQueue(label: "etiket2",qos: DispatchQoS.background)
+
+queue1.async { // async
+    for i in 1...10{
+        print("A : \(i)")
+    }
+}
+
+queue2.async {
+    for i in 100...110{
+        print("B \(i)")
+    }
+}
+
+for i in 1000...1010 {
+    print("Main : \(i)")
+}
+
+var gecikmeSaniye:DispatchTimeInterval = .seconds(2)
+queue1.asyncAfter(deadline: .now()+gecikmeSaniye){
+    print(Date())
+}
